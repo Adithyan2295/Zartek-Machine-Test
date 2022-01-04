c93bca0223d7e25c53a079a45bb94f49e7e1bb13
Adithyan <adithyansofficial@gmail.com>
2022-01-04 15:46:33 +0530

    Home page tabbar

--- machine_test/lib/HomePage/HomePage.dart
+++ machine_test/lib/HomePage/HomePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:machine_test/HomePage/HomePageController.dart';
 import 'package:machine_test/HomePage/HomePageModel.dart';
 import 'package:machine_test/Model/SingleTonModel.dart';
+import 'package:badges/badges.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -22,34 +23,17 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.grey),
+    return mainWidget();
+  }
+
+  Widget mainWidget() {
+    return DefaultTabController(
+      length: Singleton.singleton.menuModel[0].tableMenuList.length,
+      child: Scaffold(
+        appBar: appbar(),
+        drawer: drawer(),
+        body: body(),
       ),
-      drawer: drawer(),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(homePageModel.user.uid),
-          Text(homePageModel.user.email),
-          Text(homePageModel.user.displayName),
-          CircleAvatar(
-            backgroundImage: NetworkImage(homePageModel.user.photoURL),
-            radius: 20,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              // FirebaseAuth.instance.signOut();
-              // homePageController.removeFirebaseToken(context);
-              print(Singleton.singleton.menuModel[0].tableMenuList[0].menuCategory);
-            },
-            child: Icon(Icons.exit_to_app),
-          )
-        ],
-      )),
     );
   }
 
@@ -102,6 +86,100 @@ class _HomePageState extends State<HomePage> {
       ),
       decoration: BoxDecoration(
         color: Colors.green,
+      ),
+    );
+  }
+
+  appbar() {
+    return AppBar(
+      title: Text('Home'),
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(color: Colors.grey),
+      actions: [
+        cartIcon(),
+        SizedBox(
+          width: 10,
+        ),
+      ],
+      bottom: tabBar(),
+    );
+  }
+
+  Widget tabBar() {
+    return TabBar(
+        isScrollable: true,
+        indicatorColor: Colors.red,
+        labelStyle: TextStyle(fontSize: 12, color: Colors.red),
+        labelColor: Colors.red,
+        unselectedLabelColor: Colors.grey,
+        unselectedLabelStyle: TextStyle(fontSize: 12, color: Colors.grey),
+        tabs: tabbarList());
+  }
+
+  List<Widget> tabbarList() {
+    var temp = Singleton.singleton.menuModel[0].tableMenuList;
+    List<Widget> tab = [];
+    for (var tempMenutype in temp) {
+      tab.add(Tab(
+        text: tempMenutype.menuCategory,
+      ));
+    }
+    return tab;
+  }
+
+  Widget cartIcon() {
+    return Center(
+      child: Badge(
+        position: BadgePosition.topEnd(top: 0, end: 2),
+        animationDuration: Duration(milliseconds: 300),
+        animationType: BadgeAnimationType.slide,
+        badgeContent: Text(
+          '7',
+          style: TextStyle(color: Colors.white),
+        ),
+        child: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}),
+      ),
+    );
+  }
+
+  body() {
+    return Stack(
+      children: [
+        Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(homePageModel.user.uid),
+            Text(homePageModel.user.email),
+            Text(homePageModel.user.displayName),
+            CircleAvatar(
+              backgroundImage: NetworkImage(homePageModel.user.photoURL),
+              radius: 20,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                // FirebaseAuth.instance.signOut();
+                // homePageController.removeFirebaseToken(context);
+                print(Singleton
+                    .singleton.menuModel[0].tableMenuList[0].menuCategory);
+              },
+              child: Icon(Icons.exit_to_app),
+            )
+          ],
+        )),
+        Positioned.fill(child: loadingIndicator())
+      ],
+    );
+  }
+
+  loadingIndicator() {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white,
+      child: Center(
+        child:
+            Container(color: Colors.white, child: CircularProgressIndicator()),
       ),
     );
   }
